5fc163ab1b735236c5d7639209f7bef0fcf9cc3b
mhmtsckr <mhmtsckr029@gmail.com>
2026-09-23 21:59:56 +0300

    Publish KEKS legal business contact information

--- lib/businessInfo.ts
+++ lib/businessInfo.ts
@@ -1,9 +1,9 @@
 export const BUSINESS_INFO={
   brandName:'KEKS Akademi',
   serviceName:'Kazandıran Eğitim ve Koçluk Sistemi',
-  legalTitle:'Mehmet Sait Çakır',
-  address:'',
-  phone:'',
+  legalTitle:'Mehmet Sait Çakır (KEKS AKADEMİ)',
+  address:'Cumhuriyet Mahallesi Piri Reis Caddesi Tuana Apartmanı No: 25 Kat:2 Daire No: 6 Adıyaman/Merkez',
+  phone:'05327081471',
   email:'keksakademi@gmail.com'
 } as const;
 
