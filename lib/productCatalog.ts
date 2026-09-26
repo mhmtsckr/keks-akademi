7aa5874403d5a7905f03e8c130254a67d3311018
mhmtsckr <mhmtsckr029@gmail.com>
2026-09-26 16:35:37 +0300

    feat: add single KEKS product catalog source

---
+++ lib/productCatalog.ts
@@ -0,0 +1,10 @@
+export const KEKS_MONTHLY_PRODUCT=Object.freeze({
+  key:'KEKS_MONTHLY_TEST',
+  baseName:'KEKS Eğilim Taraması ve Eğitim Düzeyine Göre Ön Görüşme Test Formu',
+  defaultPricing:Object.freeze({
+    listPriceKurus:80000,
+    priceKurus:40000
+  })
+});
+
+export type ProductDefinition=typeof KEKS_MONTHLY_PRODUCT;
